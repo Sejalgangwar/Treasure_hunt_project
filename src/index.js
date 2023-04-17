1f487d21aa470fcf64912378efc6950a7841c304
Sejalgangwar <sejalgangwar7@gmail.com>
2023-04-17 10:09:54 +0530

    test3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
     <App/>
-    </BrowserRouter>,document.getElementById('root')
+    </HashRouter>,document.getElementById('root')
 )
